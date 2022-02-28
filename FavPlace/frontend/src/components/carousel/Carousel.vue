84125d1acb5783582b594d89d462eb6a2e760313
Bryan Lojan <bryanlc9808@gmail.com>
2022-02-28 18:14:39 +0100

    finish infinity carousel

--- FavPlace/frontend/src/components/carousel/Carousel.vue
+++ FavPlace/frontend/src/components/carousel/Carousel.vue
@@ -7,20 +7,46 @@
           :current-slide="currentSlide"
           :index="index">
          </carousel-item>
+         <carousel-controls @prev="prev" @next="next"></carousel-controls>
       </div>
   </div>
 </template>
 <script>
 import { defineComponent } from "vue";
 import CarouselItem from "./CarouselItem.vue"
+import CarouselControls from "./CarouselControls.vue"
 
 export default defineComponent({
     name: "Carousel",
     props:['slides'],
-    components:{CarouselItem},
+    components:{CarouselItem , CarouselControls},
     data: () =>( {
-        currentSlide:0
-    })
+        currentSlide:0,
+        
+    }),
+
+    methods:{
+        setCurrentSlide(index){
+            this.currentSlide=index;
+            console.log('holaaaaaaaaaaaaaaaaaaaa',this.currentSlide)
+        },
+        prev(){
+            console.log('prev hola',this.currentSlide)
+         const index =
+        this.currentSlide > 0 ? this.currentSlide -1 : this.slides.length - 1;
+      this.setCurrentSlide(index);
+      console.log('prev adios',this.currentSlide)
+
+        },
+
+        next(){
+            console.log('next hola',this.currentSlide)
+            const index =
+        this.currentSlide < this.slides.length -1 ? this.currentSlide + 1 : 0 ;
+        this.setCurrentSlide(index);
+        console.log('next adios',this.currentSlide)
+        }
+    }
 });
 </script>
 <style>
---
+++ FavPlace/frontend/src/components/carousel/CarouselControls.vue
@@ -0,0 +1,27 @@
+<template>
+   <button class="carousel-control left"  @click="$emit('prev')">Prev</button>
+    <button class="carousel-control rigth" @click="$emit('next')">Next</button>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+    
+   emits:['prev','next']
+});
+</script>
+
+<style>
+.carousel-control{
+    position: absolute;
+    top: 50%;
+}
+
+.left{
+    left: 0;
+}
+
+.rigth{
+ right: 0;
+}
+</style>
